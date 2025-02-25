f7094348bd5df2b409677a1c948a52aae53631b1
chengmingLi <ccaihong@icloud.com>
2025-02-26 09:36:23 +1100

    NavBar开发

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
-import MyClock from '../src/my-clock';
+import NavBar from './components/nav-bar';
+import Footer from './components/footer';
+import PostList from './components/post-list';
 
 // 函数式组建
 function App() {
   return(
-  <div className="App">
-    <MyClock></MyClock>
+    // 加上data-bs-theme配置项，bootstrap会切换dark主题
+  <div className="App" data-bs-theme="dark">
+    <NavBar></NavBar>
+    <div>
+      <PostList></PostList>
+    </div>
+    <Footer></Footer>
   </div>
   )
 }
---
+++ src/components/write-post/index.jsx
@@ -0,0 +1,7 @@
+export default props=>{
+    return(
+        <div>
+            <h1>Write Post Component</h1>
+        </div>
+    )
+}
