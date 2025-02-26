2515d94de10dda9d3ecfadeb6f736d02e2b8d4cc
chengmingLi <ccaihong@icloud.com>
2025-02-27 10:40:00 +1100

    利用ajv进行表单校验

--- src/components/write-post/index.jsx
+++ src/components/write-post/index.jsx
@@ -1,4 +1,41 @@
 import {useState} from 'react';
+import Ajv from 'ajv';
+import ajvErrors from 'ajv-errors';
+//import { validate } from '../../../node_modules/webpack/types.d';
+
+
+
+const schema = {
+    type: "object",
+    properties: {
+        title: {
+            type: "string",
+            minLength: 2,
+            maxLength: 32,
+            errorMessage: {
+                type: "标题必须是字符串",
+                minLength: "标题最少2个字符",
+                maxLength: "标题最多32个字符"
+            }
+        },
+
+        content: {
+            type: "string",
+            minLength: 2,
+            maxLength: 140,
+            errorMessage: {
+                type: "内容必须是字符串",
+                minLength: "内容最少2个字符",
+                maxLength: "内容最多140个字符"
+            }
+        }
+    }
+}
+
+const ajv = new Ajv({allErrors:true});
+ajvErrors(ajv);
+const validate = ajv.compile(schema);
+
 
 export default props=>{
 
@@ -6,13 +43,15 @@ export default props=>{
         title:"",
         content:""
     });
+    const [errors, setErrors] = useState({});
+
 
     const handleChange = e => {
         // 获取用户输入
         console.log("change");
         const name = e.target.name;
         const value = e.target.value;
-
+        
         // 更新状态
         setPostDetail({
             ...postDetail, // 展开原对象
@@ -35,27 +74,38 @@ export default props=>{
 
         // 3. 数据校验（minLength, maxLength, text, number reguler expression...)
         //    自己写代码校验，失败就提示用户，不提交数据
-        
+        const isValid = validate(postDetail);
+        setErrors({});
+
+        if (!isValid) {
+            const fieldErrors = {};
+            validate?.errors.forEach((error)=>{
+                const field = error.instancePath.substring(1);
+                fieldErrors[field] = error.message;
+        });
+        setErrors(fieldErrors);
+        console.log(fieldErrors);
+        return;
         // 4. 提交数据
 
+        }
     }
-
 
     // 手动写代码校验表单数据
-    const validateForm = () => {
-        let isValid = true;
+    // const validateForm = () => {
+    //     let isValid = true;
 
-        const {title, content}=postDetail;
+    //     const {title, content}=postDetail;
 
-        if (title.length<2 || title.length>32){
-            isValid = false;
-        }
-        if (content.length>140){
-            isValid = false;
-        }
+    //     if (title.length<2 || title.length>32){
+    //         isValid = false;
+    //     }
+    //     if (content.length>140){
+    //         isValid = false;
+    //     }
 
-        return isValid;
-    }
+    //     return isValid;
+    // }
 
 
 
@@ -68,17 +118,19 @@ export default props=>{
                         <input
                             name="title" 
                             type="text" 
-                            className="form-control" 
+                            className={`form-control ${errors.title ? 'is-invalid' : ""}`}
                             placeholder=" 请输入标题，2-32个字符。"
                             value={postDetail.title}
                             onChange={handleChange}/>
+                        
+                        {errors.title && <span className="text-danger">{errors.title}</span>}
                     </div>
 
                     <div className="mb-3">
                         <label className="form-label">内容：</label>
                         <textarea 
                             name="content"
-                            className="form-control" 
+                            className={`form-control ${errors.content ? 'is-invalid' : ""}`}
                             rows="10" 
                             placeholder="请输入内容，最长140个字符。"
                             value={postDetail.content}
@@ -89,7 +141,7 @@ export default props=>{
                     <button 
                         type="submit" 
                         className="btn btn-primary"
-                        onClick={validateForm}>
+                        >
                         提交
                     </button>
                 </form>
